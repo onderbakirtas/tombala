afe119b36217431cdcedfa629e03fa08115917f5
Onder Bakirtas <onderbakirtas@gmail.com>
2022-12-24 05:06:47 +0300

    feat: init

---
+++ src/lib/utils.ts
@@ -0,0 +1,15 @@
+export const shuffleArray = (array: number[]) => {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		const temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+};
+
+export const bingoChecker = (bingo: number[][]) => {
+	const row1Length = bingo.map((b) => b[0] !== 0).filter((e) => e).length;
+	const row2Length = bingo.map((b) => b[1] !== 0).filter((e) => e).length;
+
+	return row1Length === 5 && row2Length === 5;
+};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,147 @@
+<script lang="ts">
+	import { bingoChecker, shuffleArray } from '$lib/utils';
+	import { onMount } from 'svelte';
+
+	let nonDrawable = true;
+
+	let splittedNumbers: number[][] = [];
+
+	const depth = Array.from(Array(9).keys());
+
+	const m = Math.floor(Math.random() * 2) + 1;
+
+	depth.forEach((d) => (splittedNumbers[d] = []));
+
+	splittedNumbers = splittedNumbers.map((_, i) => {
+		_ = Array.from(Array(10).keys(), (e) => e + i * 10);
+
+		if (i === 0) _.shift();
+
+		if (i === depth.length - 1) _.push(90);
+
+		return _;
+	});
+
+	let bingoNumbers: number[][] = [];
+
+	onMount(() => {
+		while (nonDrawable) {
+			bingoNumbers = splittedNumbers.map((s, j) => {
+				const splitter = (j % 2) + 1;
+
+				let item: number[] = [];
+				let i = 0;
+				let sLength = s.length;
+
+				do {
+					i += 1;
+					sLength -= 1;
+					const random = s[Math.floor(Math.random() * sLength)];
+					item.push(random);
+					s = s.filter((e) => e !== random);
+				} while (i < 3);
+
+				item = item.sort((a, b) => a - b);
+
+				if (m % 2 === 0) {
+					if (j === splittedNumbers.length - 2) {
+						item = item.splice(splitter - 1, splitter);
+					} else {
+						item = item.splice(splitter, splitter + 1);
+					}
+				} else {
+					if (j === splittedNumbers.length - 1 || j == 2) {
+						item = item.splice(splitter, splitter + 1);
+					} else {
+						item = item.splice(splitter - 1, splitter);
+					}
+				}
+
+				item = item.concat(Array(3 - item.length).fill(0));
+
+				shuffleArray(item);
+
+				return item;
+			});
+
+			nonDrawable = !bingoChecker(bingoNumbers);
+
+			console.log(nonDrawable);
+		}
+	});
+</script>
+
+{#if !nonDrawable}
+	<div class="card">
+		<header class="card-header">
+			<div>Tombala</div>
+			<div class="card-number">1</div>
+		</header>
+		<main class="card-main">
+			{#each bingoNumbers as column}
+				<div class="column">
+					{#each column as item}
+						<div class="box" class:filled={item}>{item || ''}</div>
+					{/each}
+				</div>
+			{/each}
+		</main>
+	</div>
+{/if}
+
+<style>
+	.card {
+		display: inline-flex;
+		flex-direction: column;
+		border: 0.5rem solid tomato;
+		border-radius: 0.5rem;
+	}
+
+	.card-header {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		background-color: tomato;
+		color: white;
+		line-height: 1;
+		height: 3rem;
+		padding: 0 1rem 0.5rem;
+	}
+
+	.card-number {
+		height: 2rem;
+		width: 2rem;
+		display: inline-flex;
+		align-items: center;
+		justify-content: center;
+		border: 1px solid white;
+		border-radius: 50%;
+	}
+
+	.card-main {
+		display: flex;
+		border: 1px solid white;
+	}
+
+	.column {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.box {
+		width: 4rem;
+		height: 4rem;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+		font-size: 2rem;
+		font-weight: 700;
+		margin: 0.125rem;
+		background-color: tomato;
+	}
+
+	.box.filled {
+		background-color: white;
+	}
+</style>
